4e7ced1ebc3305a2f81c97b86c25ff4fc65b720c
FaizX786 <faiziqbal733@gmail.com>
2022-05-05 18:11:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer"
 
 
